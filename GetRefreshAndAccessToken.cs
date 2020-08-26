41041ffa8a851d146dfa7aa2a5733a9a7c68cbf5
duc <duc@bookbotkids.com>
2020-08-26 17:16:18 +0700

    Refactor rename function GetAccessToken => GetRefreshAndAccessToken

--- GetRefreshAndAccessToken.cs
+++ GetRefreshAndAccessToken.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Authentication.Shared.Models;
-using Authentication.Shared.Services;
 using Authentication.Shared.Library;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,26 +11,26 @@ using Microsoft.Extensions.Logging;
 namespace Authentication
 {
     /// <summary>
-    /// Get b2c access token azure function
-    /// This function uses to get a b2c access token from email/password or id token (from b2c login)
+    /// Get b2c access token and refresh token azure function
+    /// This function uses to get a b2c access token and refresh token from email/password or id token (from b2c login)
     /// </summary>
-    public class GetAccessToken: BaseFunction
+    public class GetRefreshAndAccessToken : BaseFunction
     {
         /// <summary>
-        /// A http (Get, Post) method to get user access token by email and password or id token (from b2c login).<br/>
+        /// A http (Get, Post) method to get user access token and refresh token by email and password or id token (from b2c login).<br/>
         /// Parameters:<br/>
         /// <list type="bullet">
         /// <item><description>"id_token": The id token from b2c login</description></item>
         /// <item><description>"email": user email</description></item>
         /// <item><description>"password": user password</description></item>
         /// </list> 
-        /// If id_token exists, then validate it with b2c custom policy (then get its email) and get the b2c access token by using its email and generated password <br/>
-        /// If email and password exist, then get b2c access token by them 
+        /// If id_token exists, then validate it with b2c custom policy (then get its email) and get the b2c access token and refresh token by using its email and generated password <br/>
+        /// If email and password exist, then get b2c access token and refresh token by them 
         /// </summary>
         /// <param name="req">HttpRequest type. It does contains parameters, headers...</param>
         /// <param name="log">The logger instance</param>
-        /// <returns>Access token result with http code 200 if no error, otherwise return http error</returns> 
-        [FunctionName("GetAccessToken")]
+        /// <returns>access token and refresh token result with http code 200 if no error, otherwise return http error</returns> 
+        [FunctionName("GetRefreshAndAccessToken")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -41,18 +40,18 @@ namespace Authentication
             string idToken = req.Query["id_token"];
             if (!string.IsNullOrWhiteSpace(idToken))
             {
-                return await GetAccessTokenFromIdToken(idToken);
+                return await GetFromIdToken(idToken);
             }
 
-            return await GetAccessTokenFromLogin(req.Query["email"], req.Query["password"]);
+            return await GetFromEmailPassword(req.Query["email"], req.Query["password"]);
         }
 
         /// <summary>
-        /// Get b2c access token by id token
+        /// Get b2c access token and refresh token by id token
         /// </summary>
         /// <param name="idToken">id token</param>
-        /// <returns>Access token result with http code 200 if no error, otherwise return http error</returns>
-        private static async Task<IActionResult> GetAccessTokenFromIdToken(string idToken)
+        /// <returns>access token and refresh token result with http code 200 if no error, otherwise return http error</returns>
+        private static async Task<IActionResult> GetFromIdToken(string idToken)
         {
             // validate id token and get the email
             var (result, message, email) = await ADAccess.Instance.ValidateIdToken(idToken);
@@ -63,7 +62,7 @@ namespace Authentication
 
             try
             {
-                // using email (and generated password) to get access token
+                // using email (and generated password) to get access token and refresh token
                 var token = await ADAccess.Instance.GetAccessToken(email);
                 if (token == null)
                 {
@@ -86,22 +85,22 @@ namespace Authentication
                     }
                 }
 
-                // return access token result
+                // return access token and refresh token result
                 return new JsonResult(new { success = true, token, group }) { StatusCode = StatusCodes.Status200OK };
             }
             catch (Exception)
             {
-                return CreateErrorResponse("Can not generate access token", StatusCodes.Status500InternalServerError);
+                return CreateErrorResponse("Can not generate access token and refresh token", StatusCodes.Status500InternalServerError);
             }
         }
 
         /// <summary>
-        /// Get b2c access token from email and password
+        /// Get b2c access token and refresh token from email and password
         /// </summary>
         /// <param name="email">user email</param>
         /// <param name="password">user password</param>
-        /// <returns>Access token result with http code 200 if no error, otherwise return http error</returns>
-        private static async Task<IActionResult> GetAccessTokenFromLogin(string email, string password)
+        /// <returns>access token and refresh token result with http code 200 if no error, otherwise return http error</returns>
+        private static async Task<IActionResult> GetFromEmailPassword(string email, string password)
         {
             // validate email and password
             if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
@@ -109,14 +108,14 @@ namespace Authentication
                 return CreateErrorResponse("email & password are required");
             }
 
-            // get b2c access token by using email and password
+            // get b2c access token and refresh token by using email and password
             var token = await ADAccess.Instance.GetAccessToken(email, password);
             if (token == null)
             {
-                return CreateErrorResponse("Cannot get access token. Make sure email and password are correct", StatusCodes.Status500InternalServerError);
+                return CreateErrorResponse("Cannot get access token and refresh token. Make sure email and password are correct", StatusCodes.Status500InternalServerError);
             }
 
-            // return access token result
+            // return access token and refresh token result
             return new JsonResult(new { success = true, token }) { StatusCode = StatusCodes.Status200OK };
         }
     }
