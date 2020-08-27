9a7cc4a81a79227675e628ee745b5c4268476527
duc <duc@bookbotkids.com>
2020-08-27 17:56:52 +0700

    Add unit test

---
+++ Authentication.Tests/CheckAccountTest.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Xunit;
+
+namespace Authentication.Tests
+{
+    public class CheckAccountTest
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+
+        [Fact]
+        public async Task TestEmailInvalid()
+        {
+            Dictionary<string, StringValues> parameters = new Dictionary<string, StringValues>();
+            var request = TestFactory.CreateHttpRequest(parameters);
+            var response = (JsonResult)await CheckAccount.Run(request, logger);
+            Assert.Equal(400, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestSuccess()
+        {
+            Dictionary<string, StringValues> parameters = new Dictionary<string, StringValues>()
+            {
+                { "email", "test@gmail.com"}
+            };
+
+            var request = TestFactory.CreateHttpRequest(parameters);
+            var response = (JsonResult)await CheckAccount.Run(request, logger);
+            Assert.Equal(200, response.StatusCode);
+        }
+    }
+}
---
+++ Authentication.Tests/TestFactory.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace Authentication.Tests
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        private NullScope() { }
+
+        public void Dispose() { }
+    }
+
+    public class ListLogger : ILogger
+    {
+        public IList<string> Logs;
+
+        public IDisposable BeginScope<TState>(TState state) => NullScope.Instance;
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public ListLogger()
+        {
+            Logs = new List<string>();
+        }
+
+        public void Log<TState>(LogLevel logLevel,
+                                EventId eventId,
+                                TState state,
+                                Exception exception,
+                                Func<TState, Exception, string> formatter)
+        {
+            string message = formatter(state, exception);
+            Logs.Add(message);
+        }
+    }
+
+    public class TestFactory
+    {
+        public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)
+        {
+            ILogger logger;
+
+            if (type == LoggerTypes.List)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+
+            return logger;
+        }
+
+        public static HttpRequest CreateHttpRequest(Dictionary<string, StringValues> parameters)
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            request.Query = new QueryCollection(parameters);
+            return request;
+        }
+    }
+}
